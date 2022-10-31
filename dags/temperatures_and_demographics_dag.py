bb9663f0b3b573c61dbcae11b95970ded7f1f77e
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-31 14:27:44 +0200

    Created the project DAG : )

--- dags/temperatures_and_demographics_dag.py
+++ dags/temperatures_and_demographics_dag.py
@@ -20,35 +20,36 @@ default_args = {
   'catchup': False
 }
 
-dag = DAG(
-  'temperatures_and_demographics_dag',
-  default_args = default_args,
-  description = '''
-    Stage data from an S3 bucket to a redshift cluster,
-    and then moves the data from the staged tables to the database tables,
-    and then feeds the data to a linear regression model
-    to analyze the relation between the two variables
-    ''',
-  schedule_interval = '@yearly'
+dag = \
+  DAG(
+    'temperatures_and_demographics_dag',
+    default_args = default_args,
+    description = '''
+      Stage data from an S3 bucket to a redshift cluster,
+      and then moves the data from the staged tables to the database tables,
+      and then feeds the data to a linear regression model
+      to analyze the relation between the two variables
+      ''',
+    schedule_interval = '@yearly'
   )
 
 start_operator = DummyOperator(task_id = 'Begin_execution',  dag = dag)
 
 create_staging_temperatures_table = \
   CreateTableOperator(
-  task_id = 'create_staging_temperatures_table',
-  dag = dag,
-  redshift_conn_id = 'redshift',
-  table = 'staging_events',
-  create_sql = SqlQueries.staging_temperatures_create_sql
+    task_id = 'create_staging_temperatures_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'staging_events',
+    create_sql = SqlQueries.staging_temperatures_create_sql
   )
 
 create_staging_demographics_table = \
   CreateTableOperator(
-  task_id = 'create_staging_demographics_table',
-  dag = dag,
-  redshift_conn_id = 'redshift',
-  create_sql = SqlQueries.staging_demographics_create_sql
+    task_id = 'create_staging_demographics_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    create_sql = SqlQueries.staging_demographics_create_sql
   )
 
 aws_hook = AwsHook('aws_credentials')
@@ -56,27 +57,96 @@ aws_credentials = aws_hook.get_credentials()
 
 stage_temperatures_to_redshift = \
   StageToRedshiftOperator(
-  task_id = 'stage_temperatures',
-  dag = dag,
-  redshift_conn_id = 'redshift',
-  aws_credentials_id = 'aws_credentials',
-  region = 'us-west-2',
-  table = 'staged_temperatures',
-  s3_bucket = 'tempratures-and-demographics',
-  s3_key = 'temperatures-data'
+    task_id = 'stage_temperatures',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    aws_credentials_id = 'aws_credentials',
+    region = 'us-west-2',
+    table = 'staged_temperatures',
+    s3_bucket = 'tempratures-and-demographics',
+    s3_key = 'temperatures-data'
   )
 
 stage_demographics_to_redshift = \
   StageToRedshiftOperator(
-  task_id = 'stage_demographgics',
-  dag = dag,
-  redshift_conn_id = 'redshift',
-  aws_credentials_id = 'aws_credentials',
-  region = 'us-west-2',
-  table = 'staged_demographics',
-  delimiter = ';',
-  s3_bucket = 'temperatures-and-demographics',
-  s3_key = 'demographics-data'
+    task_id = 'stage_demographgics',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    aws_credentials_id = 'aws_credentials',
+    region = 'us-west-2',
+    table = 'staged_demographics',
+    delimiter = ';',
+    s3_bucket = 'temperatures-and-demographics',
+    s3_key = 'demographics-data'
+  )
+
+create_temperatures_demographics_fact_table = \
+  CreateTableOperator(
+    task_id = 'create_temperatures_demographics_fact_table',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'temperatures_demographics_fact',
+    create_sql = SqlQueries.temperatures_demographics_fact_table_create_sql
+  )
+
+create_race_temperatures_view = \
+  CreateTableOperator(
+    task_id = 'create_race_temperatures_view',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'race_temperatures_view',
+    create_sql = SqlQueries.race_temperatures_view_create_sql
+  )
+
+create_gender_temperatures_view = \
+  CreateTableOperator(
+    task_id = 'create_gender_temperatures_view',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'gender_temperatures_view',
+    create_sql = SqlQueries.gender_temperatures_view_create_sql
+  )
+
+create_age_temperatures_view = \
+  CreateTableOperator(
+    task_id = 'create_age_temperatures_view',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    table = 'age_temperatures_view',
+    create_sql = SqlQueries.age_temperatures_view_create_sql
+  )
+
+run_data_quality_checks = \
+  DataQualityOperator(
+    task_id = '',
+    dag = dag,
+    redshift_conn_id = 'redshift',
+    test_count_query = SqlQueries.data_quality_sql
   )
 
 end_operator = DummyOperator(task_id = 'stop_execution',  dag = dag)
+
+# First Stage
+start_operator >> create_staging_temperatures_table
+start_operator >> create_staging_demographics_table
+
+# Second Stage
+create_staging_temperatures_table >> stage_temperatures_to_redshift
+create_staging_demographics_table >> stage_demographics_to_redshift
+
+# Third Stage
+stage_temperatures_to_redshift >> run_data_quality_checks
+stage_demographics_to_redshift >> run_data_quality_checks
+
+# Fourth Stage
+run_data_quality_checks >> create_age_temperatures_view
+
+# Fifth Stage
+create_temperatures_demographics_fact_table >> create_age_temperatures_view
+create_temperatures_demographics_fact_table >> create_gender_temperatures_view
+create_temperatures_demographics_fact_table >> create_race_temperatures_view
+
+# Sixth (and last) stage
+create_age_temperatures_view >> end_operator
+create_gender_temperatures_view >> end_operator
+create_race_temperatures_view >> end_operator
--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,53 +1,98 @@
 class SqlQueries:
     staging_temperatues_create_sql = ('''
         CREATE TABLE public.staged_temperatures (
-        date DATE
-        average_temprature NUMERIC(18,0)
-        average_temprature_unertainty NUMERIC(18,0)
-        city VARCHAR(256)
-        country VARCHAR(256)
-        latitude VARCHAR(256)
+        date DATE,
+        average_temprature NUMERIC(18,0) NOT NULL,
+        average_temprature_unertainty NUMERIC(18,0),
+        city VARCHAR(256) NOT NULL,
+        country VARCHAR(256),
+        latitude VARCHAR(256),
         longitude VARCHAR(256)
         );
     ''')
 
     staging_demographics_create_sql = ('''
         CREATE TABLE public.staged_demographics (
-        city VARCHAR(256)
-        state VARCHAR(256)
-        median_age NUMERIC(18,0)
-        male_population INT4
-        female_population INT4
-        total_population INT4
-        number_of_veterans INT4
-        foreign-born INT4
-        average_household_size NUMERIC(18,0)
-        state_code VARCHAR(2)
-        race VARCHAR(256)
+        city VARCHAR(256),
+        state VARCHAR(256),
+        median_age NUMERIC(18,0),
+        male_population INT4,
+        female_population INT4,
+        total_population INT4,
+        number_of_veterans INT4,
+        foreign-born INT4,
+        average_household_size NUMERIC(18,0),
+        state_code VARCHAR(2),
+        race VARCHAR(256),
         count INT4
         );
     ''')
 
-    songplays_table_create_sql = ('''
-    
+    temperatures_demographics_fact_table_create_sql = ('''
+    CREATE TABLE IF NOT EXISTS public.temperatures_demographics_fact_table
+    AS SELECT
+        date,
+        city,
+        state,
+        state_code,
+        latitude,
+        longitude,
+        average_temperature,
+        median_age,
+        total_population,
+        male_population,
+        female_population,
+        race,
+        count,
+        foreign-born
+
+    FROM public.staged_temperatures LEFT JOIN public.staged_demographics
+    ON staged_temperatures.city = staged_demographics.city
+
+    WHERE staged_temperatures.country = 'United States'
+    WITH NO SCHEMA BINDING;
     ''')
 
-    users_table_create_sql = ('''
+    race_temperatures_view_create_sql = ('''
+    CREATE OR REPLACE VIEW public.race_temperatures_view
+    AS SELECT
+        race,
+        count,
+        city,
+        state_code,
+        average_temperature
     
+    FROM public.temperatures_demographics_fact_table;
     ''')
 
-    songs_table_create_sql = ('''
+    gender_temperatures_view_create_sql = ('''
+    CREATE OR REPLACE VIEW public.race_temperatures_view
+    AS SELECT
+        male_population,
+        female_population,
+        city,
+        state_code,
+        average_temperature
     
+    FROM public.temperatures_demographics_fact_table;
     ''')
 
-    artists_table_create_sql = ('''
+    age_temperatures_view_create_sql = ('''
+    CREATE OR REPLACE VIEW public.race_temperatures_view
+    AS SELECT
+        median_age,
+        city,
+        state_code,
+        average_temperature
     
+    FROM public.temperatures_demographics_fact_table;
     ''')
 
-    time_table_create_sql = ('''
+    data_quality_sql = ('''
+    SELECT COUNT (*)
 
-    ''')
-   
-    userId_data_quality_check = ("""
+    FROM public.staged_temperatures LEFT JOIN public.staged_demographics
+    ON staged_temperatures.city = staged_demographics.city
     
-    """)
+    WHERE average_temperature OR city IS NULL;
+    ''')
