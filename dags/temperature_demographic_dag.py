5d883e57e1e17d257dc85e33d5e7da0ed6b7ab98
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-31 11:36:33 +0200

    Renamed the file to stage_to_redshift : )

--- dags/temperature_demographic_dag.py
+++ dags/temperature_demographic_dag.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.hooks.aws_hook import AwsHook
-from plugins.operators.stage_redshift import StageToRedshiftOperator
+from plugins.operators.stage_to_redshift import StageToRedshiftOperator
 from plugins.operators.load_tables import LoadTableOperator
 from plugins.operators.data_quality_checks import DataQualityOperator
 from plugins.operators.creat_tables import CreateTableOperator
